17e733427658eacdd314d8962915ecef694b89b3
Martin Podloucký <podlouckymartin@gmail.com>
2019-01-31 11:05:25 +0100

    Stage 1: Moving to the right.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
+const GRID_SIZE = 85;
+
+let xpos = 0;
+let mouth = 'open';
+
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('it works');
+  document.addEventListener('keydown', (e) => {
+    if(e.keyCode === 39) { // Arrow right
+      if(mouth === 'open') {
+        mouth = 'close';
+      } else {
+        mouth = 'open';
+      }
+      xpos++;
+  
+      let pac = document.querySelector('.pac');
+      
+      if(mouth === 'open') {
+        pac.style.backgroundPositionX = '0px';
+      } else {
+        pac.style.backgroundPositionX = GRID_SIZE + 'px';
+      }
+      pac.style.left = xpos * GRID_SIZE + 'px';
+    }
+  })
 });
